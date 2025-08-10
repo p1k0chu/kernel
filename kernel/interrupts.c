be26cf021a7ba21bf1df28c57d6e523eaa3e71cb
p1k0chu <anatoliyid0207@gmail.com>
2025-08-11 00:26:35 +0300

    dump a lot of useful info in exception handler
    * dump stack in hex

--- kernel/interrupts.c
+++ kernel/interrupts.c
@@ -15,23 +15,79 @@ void setup_idt() {
     load_idtr(&idtr);
 }
 
-void exc_handler(int code) {
-    const char msg[] = "Cpu fault: ";
-    short     *vga   = (void *)0xB8000;
+static void printstr(short **const vga, const char *str, uint32_t size) {
+    for (uint8_t i = 0; i < size; ++i) {
+        *((*vga)++) = str[i] | ((WHITE | BLUE_BG) << 8);
+    }
+}
+
+static uint8_t itoc(uint8_t i, uint8_t base) {
+    char    c;
+    uint8_t rem = i % base;
+    if (rem >= 10) {
+        c = 'A';
+        rem -= 10;
+    } else {
+        c = '0';
+    }
+    return c + rem;
+}
+
+static void printnum(short *vga, uint32_t number, uint8_t base) {
+    uint16_t i = 0;
+    for (uint32_t n = number; n >= base; n /= base) ++i;
+
+    for (; number >= base; number /= base) {
+        vga[i--] = itoc(number, base) | ((WHITE | BLUE_BG) << 8);
+    }
+    vga[i] = itoc(number, base) | ((WHITE | BLUE_BG) << 8);
+}
+
+void exc_handler(int                    vector,
+                 struct interrupt_frame interrupts,
+                 uint32_t               code,
+                 struct pushad_frame    registers) {
+    const char msg[]            = "Cpu fault: ";
+    const char msg2[]           = "Code, if any: ";
+    const char msg3[]           = "Instruction address: ";
+    const char msg4[]           = "CS: ";
+    const char msg5[]           = "EFLAGS: ";
+    const char msg_stack_dump[] = "Stack hex dump:";
+
+    short *vga = (void *)0xB8000;
 
     memsetw(vga, ' ' | (BLUE_BG << 8), 80 * 25);
+    vga += 81;
 
-    for (uint8_t i = 0; i < sizeof(msg) - 1; ++i) {
-        *(vga++) = msg[i] | ((WHITE | BLUE_BG) << 8);
-    }
+    printstr(&vga, msg, sizeof(msg) - 1);
+    printnum(vga, vector, 10);
 
-    memsetw(vga, ' ' | (BLUE_BG << 8), 2);
-    vga += 2;
-    while (code >= 10) {
-        *(vga--) = ('0' + code % 10) | ((WHITE | BLUE_BG) << 8);
-        code /= 10;
+    vga = (short *)0xB8002 + 80 * 2;
+    printstr(&vga, msg2, sizeof(msg2) - 1);
+    printnum(vga, code, 10);
+
+    vga = (short *)0xB8002 + 80 * 3;
+    printstr(&vga, msg3, sizeof(msg3) - 1);
+    printnum(vga, interrupts.eip, 16);
+
+    vga = (short *)0xB8002 + 80 * 4;
+    printstr(&vga, msg4, sizeof(msg4) - 1);
+    printnum(vga, interrupts.cs, 16);
+
+    vga = (short *)0xB8002 + 80 * 5;
+    printstr(&vga, msg5, sizeof(msg5) - 1);
+    printnum(vga, interrupts.eflags, 16);
+
+    vga = (short *)0xB8002 + 80 * 7;
+    printstr(&vga, msg_stack_dump, sizeof(msg_stack_dump) - 1);
+
+    vga           = (short *)0xB8000 + 80 * 8 + 3;
+    uint32_t *esp = (void *)registers.esp;
+    for (uint32_t i = 0; i < 40; ++i) {
+        const uint8_t x = i / 10;
+        const uint8_t y = i % 10;
+        printnum(vga + y * 80 + x * 10, esp[i], 16);
     }
-    *vga = ('0' + code) | ((WHITE | BLUE_BG) << 8);
 
     asm volatile("cli");
     for (;;) {
--- kernel/interrupts.h
+++ kernel/interrupts.h
@@ -2,10 +2,33 @@
 
 #include "idt.h"
 
+struct interrupt_frame {
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+};
+
+struct pushad_frame {
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+};
+
 extern idt_t  idt[];
 extern idtr_t idtr;
 extern void  *isr_stub_table[];
 
 void setup_idt();
-void exc_handler(int code);
+
+// vector is the exception index
+// code is optional error code (some of them have it)
+void exc_handler(int                    vector,
+                 struct interrupt_frame interrupts,
+                 uint32_t               code,
+                 struct pushad_frame    registers);
 
