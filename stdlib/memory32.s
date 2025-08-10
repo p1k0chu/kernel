d4360c94fa724186cd8de63da776ac7818069efd
p1k0chu <anatoliyid0207@gmail.com>
2025-08-10 13:45:44 +0300

    add memcpy

--- stdlib/memory32.s
+++ stdlib/memory32.s
@@ -3,6 +3,7 @@ bits 32
 section .text
     global memset
     global memsetw
+    global memcpy
 
 ; 3 arguments: dst, b, n
 ; fills n bytes at dst with value b
@@ -38,6 +39,29 @@ memsetw:
     rep stosw
 
     mov edi, [ebp-4]
+    mov eax, [ebp+8]
+
+    leave
+    ret
+
+; 3 arguments: dst, src, n
+; copies n bytes from src to dst
+memcpy:
+    push ebp
+    mov ebp, esp
+
+    push edi
+    push esi
+
+    cld
+    mov edi, [ebp+8]
+    mov esi, [ebp+12]
+    mov ecx, [ebp+16]
+
+    rep movsb
+
+    mov edi, [ebp-4]
+    mov esi, [ebp-8]
     mov eax, [ebp+8]
 
     leave
